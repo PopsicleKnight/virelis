6d0dca9947b755e5f7969d876129c1b41f112929
Kevin Tempels <Kevin.Tempels@esc.be>
2025-03-10 15:09:12 +0100

    Setup basic Elixir project

---
+++ lib/virelis.ex
@@ -0,0 +1,18 @@
+defmodule Virelis do
+  @moduledoc """
+  Documentation for `Virelis`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Virelis.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/virelis_test.exs
@@ -0,0 +1,8 @@
+defmodule VirelisTest do
+  use ExUnit.Case
+  doctest Virelis
+
+  test "greets the world" do
+    assert Virelis.hello() == :world
+  end
+end
